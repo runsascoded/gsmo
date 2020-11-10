e17b96e83318289db70054539fbe37c74450ba91
Ryan Williams <ryan@runsascoded.com>
2020-11-10 17:18:48 -0500

    better encapsulate gsmo-entrypoint main()

--- gsmo/entrypoint.py
+++ gsmo/entrypoint.py
@@ -6,33 +6,30 @@ from os import chdir, getcwd
 
 from .cli import run_args, load_run_config
 from .config import Config, DEFAULT_RUN_NB, DEFAULT_NB_DIR
-
-
-parser = ArgumentParser()
-parser.add_argument('--progress',action='store_true',help="When set, have papermill show progress bars")
-for arg in run_args:
-    parser.add_argument(*arg.args, **arg.kwargs)
-
-args = parser.parse_args()
-
-config = Config(args)
-get = partial(Config.get, config)
-
-nb = get('run', DEFAULT_RUN_NB)
-out = get('out', DEFAULT_NB_DIR)
-
-run_config = load_run_config(args)
-commit = config.get('commit', True)
-print(f'commit: {commit} ({args.commit})')
-
-progress_bar = args.progress
-
-dir = get('dir')
-if dir: chdir(dir)
-
 from .papermill import execute
 
 def main():
+    parser = ArgumentParser()
+    parser.add_argument('--progress',action='store_true',help="When set, have papermill show progress bars")
+    for arg in run_args:
+        parser.add_argument(*arg.args, **arg.kwargs)
+
+    args = parser.parse_args()
+
+    config = Config(args)
+    get = partial(Config.get, config)
+
+    nb = get('run', DEFAULT_RUN_NB)
+    out = get('out', DEFAULT_NB_DIR)
+
+    run_config = load_run_config(args)
+    commit = config.get('commit', True)
+
+    progress_bar = args.progress
+
+    dir = get('dir')
+    if dir: chdir(dir)
+
     execute(
         input=nb,
         output=out,
